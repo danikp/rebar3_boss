063d446cd39cd4303951efcc2a04e2578f42c937
Dmitry <dmitry.polyanovsky@gmail.com>
2017-01-28 22:08:39 +0200

    wip

--- src/rebar3_boss.erl
+++ src/rebar3_boss.erl
@@ -6,3 +6,18 @@
 init(State) ->
     {ok, State1} = rebar3_boss_prv:init(State),
     {ok, State1}.
+
+
+init(State) ->
+    inets:start(httpc, [{profile, hex}]),
+    rebar3_hex_http:maybe_setup_proxy(),
+    lists:foldl(fun provider_init/2, {ok, State}, [rebar3_boss_compile
+                                                  ,rebar3_boss_start
+                                                  ,rebar3_boss_start_dev
+                                                  ,rebar3_boss_stop
+                                                  %,rebar3_boss_reload
+                                                  %,rebar3_boss_attach
+                                                  ]).
+
+provider_init(Module, {ok, State}) ->
+Module:init(State).
