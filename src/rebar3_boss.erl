042012d9e8c740f78d7d91086625d94b2bed3844
Dmitry <dmitry.polyanovsky@gmail.com>
2018-04-21 17:22:46 +0300

    update

--- src/rebar3_boss.erl
+++ src/rebar3_boss.erl
@@ -4,15 +4,14 @@
 
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
-    inets:start(httpc, [{profile, hex}]),
-    rebar3_hex_http:maybe_setup_proxy(),
-    lists:foldl(fun provider_init/2, {ok, State}, [rebar3_boss_compile
-                                                  ,rebar3_boss_start
-                                                  ,rebar3_boss_start_dev
-                                                  ,rebar3_boss_stop
-                                                  %,rebar3_boss_reload
-                                                  %,rebar3_boss_attach
-                                                  ]).
+    lists:foldl(fun provider_init/2, {ok, State}, [
+        rebar3_boss_compile
+        ,rebar3_boss_start
+        ,rebar3_boss_start_dev
+        ,rebar3_boss_stop
+        %,rebar3_boss_reload
+        %,rebar3_boss_attach
+    ]).
 
 provider_init(Module, {ok, State}) ->
-Module:init(State).
+    Module:init(State).
